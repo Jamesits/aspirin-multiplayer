d9d85b8444706a47ba397611eb61f9f5a419a875
James Swineson <i@swineson.me>
2016-12-28 13:47:46 +0800

    better casting

--- server/server.c
+++ server/server.c
@@ -12,37 +12,35 @@ SOCKET sListen1;
 SOCKADDR *conn1;
 SOCKET sListen2;
 SOCKADDR *conn2;
-SOCKADDR_IN broadcastAddr1;
-SOCKADDR_IN broadcastAddr2;
+SOCKADDR_IN clientAddr1;
+SOCKADDR_IN clientAddr2;
+bool client1OK = false;
+bool client2OK = false;
 
 void *t1(void *a) {
     while (1) {
-        SOCKADDR_IN clientAddr;
-        memset(&clientAddr, 0, sizeof(clientAddr));
-
         char message[1024] = {0};
         socklen_t address_len = NSLEndpointV4SocketLen;
-        int recvBytes = recvfrom(sListen1, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
+        int recvBytes = recvfrom(sListen1, message, sizeof(message), 0, (struct sockaddr*)&clientAddr1, &address_len);
         if(recvBytes <= 0) continue;
+        client1OK = true;
         printf("Received from 1: %s\n", message);
-        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr1, address_len);
-        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr2, address_len);
+        if (client1OK) sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr1, address_len);
+        if (client2OK) sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr2, address_len);
     }
     return 0;
 }
 
 void *t2(void *a) {
     while (1) {
-        SOCKADDR_IN clientAddr;
-        memset(&clientAddr, 0, sizeof(clientAddr));
-
         char message[1024] = {0};
         socklen_t address_len = NSLEndpointV4SocketLen;
-        int recvBytes = recvfrom(sListen2, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
+        int recvBytes = recvfrom(sListen2, message, sizeof(message), 0, (struct sockaddr*)&clientAddr2, &address_len);
         if(recvBytes <= 0) continue;
+        client2OK = true;
         printf("Received from 2: %s\n", message);
-        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr1, address_len);
-        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr2, address_len);
+        if (client1OK) sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr1, address_len);
+        if (client2OK) sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr2, address_len);
     }
     return 0;
 }
@@ -50,15 +48,9 @@ void *t2(void *a) {
 int main(int argc, char *argv[])
 {
     char loopch=0;
-    memset(&broadcastAddr1, 0, sizeof(broadcastAddr1));
-    broadcastAddr1.sin_family = AF_INET;
-    broadcastAddr1.sin_addr.s_addr = inet_addr("255.255.255.255");
-    broadcastAddr1.sin_port = htons(PORT1);
-    memset(&broadcastAddr2, 0, sizeof(broadcastAddr2));
-    broadcastAddr2.sin_family = AF_INET;
-    broadcastAddr2.sin_addr.s_addr = inet_addr("255.255.255.255");
-    broadcastAddr2.sin_port = htons(PORT2);
    	NSLInit();
+    memset(&clientAddr1, 0, sizeof(clientAddr1));
+    memset(&clientAddr2, 0, sizeof(clientAddr2));
 	sListen1 = NSLSocket(AF_INET, SOCK_DGRAM, 0);
 	conn1 = NSLEndpointV4("0.0.0.0", PORT1);
     setsockopt(sListen1, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch));
