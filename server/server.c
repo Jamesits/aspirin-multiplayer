43c2692d9ef7cf43b230c9fa5c5e82fd4f06d638
James Swineson <i@swineson.me>
2016-12-28 13:17:01 +0800

    broadcast data

--- server/server.c
+++ server/server.c
@@ -12,6 +12,8 @@ SOCKET sListen1;
 SOCKADDR *conn1;
 SOCKET sListen2;
 SOCKADDR *conn2;
+SOCKADDR broadcastAddr1;
+SOCKADDR broadcastAddr2;
 
 void *t1(void *a) {
     while (1) {
@@ -23,8 +25,8 @@ void *t1(void *a) {
         int recvBytes = recvfrom(sListen1, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
         if(recvBytes <= 0) continue;
         printf("Received from 1: %s\n", message);
-        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
-        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr1, address_len);
+        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr2, address_len);
     }
     return 0;
 }
@@ -39,20 +41,31 @@ void *t2(void *a) {
         int recvBytes = recvfrom(sListen2, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
         if(recvBytes <= 0) continue;
         printf("Received from 2: %s\n", message);
-        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
-        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr1, address_len);
+        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&broadcastAddr2, address_len);
     }
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
+    char loopch=0;
+    memset(&broadcastAddr1, 0, sizeof(broadcastAddr1));
+    broadcastAddr1.sin_family = AF_INET;
+    broadcastAddr1.sin_addr.s_addr = inet_addr("255.255.255.255");
+    broadcastAddr1.sin_port = htons(PORT1);
+    memset(&broadcastAddr2, 0, sizeof(broadcastAddr2));
+    broadcastAddr2.sin_family = AF_INET;
+    broadcastAddr2.sin_addr.s_addr = inet_addr("255.255.255.255");
+    broadcastAddr2.sin_port = htons(PORT2);
    	NSLInit();
 	sListen1 = NSLSocket(AF_INET, SOCK_DGRAM, 0);
 	conn1 = NSLEndpointV4("0.0.0.0", PORT1);
+    setsockopt(sListen1, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch));
 	NSLBindV4(sListen1, conn1);
     sListen2 = NSLSocket(AF_INET, SOCK_DGRAM, 0);
 	conn2 = NSLEndpointV4("0.0.0.0", PORT2);
+    setsockopt(sListen2, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch));
 	NSLBindV4(sListen2, conn2);
 	printf("Server started, press ^C to quit...\n");
 
