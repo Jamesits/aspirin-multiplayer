bebc98815e877ed0763e2fd9d8b5a00cca52fce4
James Swineson <i@swineson.me>
2016-12-28 13:10:02 +0800

    do loop inside threads

--- server/server.c
+++ server/server.c
@@ -14,30 +14,34 @@ SOCKET sListen2;
 SOCKADDR *conn2;
 
 void *t1(void *a) {
-    SOCKADDR_IN clientAddr;
-    memset(&clientAddr, 0, sizeof(clientAddr));
+    while (1) {
+        SOCKADDR_IN clientAddr;
+        memset(&clientAddr, 0, sizeof(clientAddr));
 
-    char message[1024] = {0};
-    socklen_t address_len = NSLEndpointV4SocketLen;
-    int recvBytes = recvfrom(sListen1, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
-    if(recvBytes <= 0) return 0;
-    printf("Received from 1: %s\n", message);
-    sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
-    sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+        char message[1024] = {0};
+        socklen_t address_len = NSLEndpointV4SocketLen;
+        int recvBytes = recvfrom(sListen1, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
+        if(recvBytes <= 0) continue;
+        printf("Received from 1: %s\n", message);
+        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+    }
     return 0;
 }
 
 void *t2(void *a) {
-    SOCKADDR_IN clientAddr;
-    memset(&clientAddr, 0, sizeof(clientAddr));
+    while (1) {
+        SOCKADDR_IN clientAddr;
+        memset(&clientAddr, 0, sizeof(clientAddr));
 
-    char message[1024] = {0};
-    socklen_t address_len = NSLEndpointV4SocketLen;
-    int recvBytes = recvfrom(sListen2, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
-    if(recvBytes <= 0) return 0;
-    printf("Received from 2: %s\n", message);
-    sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
-    sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+        char message[1024] = {0};
+        socklen_t address_len = NSLEndpointV4SocketLen;
+        int recvBytes = recvfrom(sListen2, message, sizeof(message), 0, (struct sockaddr*)&clientAddr, &address_len);
+        if(recvBytes <= 0) continue;
+        printf("Received from 2: %s\n", message);
+        sendto(sListen1, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+        sendto(sListen2, message, recvBytes, 0, (struct sockaddr*)&clientAddr, address_len);
+    }
     return 0;
 }
 
@@ -57,7 +61,7 @@ int main(int argc, char *argv[])
     while (1){
         pthread_create(&p1, NULL, t1, NULL);
         pthread_create(&p2, NULL, t2, NULL);
-        printf("new\n");
+
         pthread_join(p1, NULL);
         pthread_join(p2, NULL);
     }
